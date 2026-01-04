dc700f25ea47aa94a9d7feaa080c16dee32161bd
Bagus Sundaru <bagussundaru@gmail.com>
2026-01-04 21:21:05 +0700

    Fix: Rename files to .jsx extensions for Vite build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import '../Layout.js' // Importing Layout to ensure global styles (if any) are loaded, though properly we should move css.
+import '../Layout.jsx' // Importing Layout to ensure global styles (if any) are loaded, though properly we should move css.
 // Assuming global styles are in index.css which we need to check, or defined in Layout.js styled components?
 // Layout.js viewed earlier had `import { Link } from 'react-router-dom'` etc.
 // Let's create a basic index.css too just in case.
